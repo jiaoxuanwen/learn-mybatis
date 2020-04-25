e994ccb460aeaa3678ebf224dce05ca6c8daa543
jiaoxuanwen <a,.2202125514>
2020-04-25 14:50:03 +0800

    新增 【mybatis注解】

---
+++ 注解-cls2/src/main/java/com/itjiao/dao/IUserDao.java
@@ -0,0 +1,23 @@
+package com.itjiao.dao;
+
+
+import com.itjiao.domain.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 用户的持久层接口
+ */
+public interface IUserDao {
+
+
+    /**
+     * 查询所有操作
+     * @return
+     */
+
+    @Select("select * from User")
+    List<User> findAll();
+
+}
